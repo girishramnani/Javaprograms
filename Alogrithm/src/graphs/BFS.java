579fb0ef507b59242af2572c51bb362e3ce9c12d
girishramnani <girishramnani95@gmail.com>
2014-11-02 22:06:14 +0530

    added the classic iterative BFS algorithm

---
+++ Alogrithm/src/graphs/BFS.java
@@ -0,0 +1,44 @@
+package graphs;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author girish
+ * this BFS alogrithm uses queue based iterative alogrithm for graphs
+ *
+ */
+public class BFS {
+	private static LinkedList<Integer> queue ;
+	private static LinkedList<Integer> visited;
+	public static void Search(Graph g,Integer num){
+		queue = new LinkedList<Integer>();
+		visited = new LinkedList<Integer>();
+		int head = g.getHead();
+		queue.add(head);
+		
+		while(!queue.isEmpty()){
+			Integer i = queue.pop();
+			if (!visited.contains(i)){
+				Iterator<Integer> iterator = g.adj(i);
+				iterator.forEachRemaining(item -> queue.add(item));
+				visited.add(i);
+				System.out.println("visited "+i);
+			}
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		Graph g = new Graph(5);
+		
+		g.addEdge(1, 2);
+		g.addEdge(2, 3);
+		g.addEdge(3, 4);
+		g.addEdge(4, 5);
+		g.addEdge(5, 1);
+		Search(g, 5);
+	}
+
+}
+
