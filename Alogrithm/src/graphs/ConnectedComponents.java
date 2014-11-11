9f5f12b41fdcd96e55da0b4f984067ef13f2af8f
girishramnani <girishramnani95@gmail.com>
2014-11-11 18:12:30 +0530

    Connected component algorithm for graph and companion DFS algo

---
+++ Alogrithm/src/graphs/ConnectedComponents.java
@@ -0,0 +1,62 @@
+package graphs;
+
+/**
+ * the connected Components algorithm works with Undirected graphs. Its algorithm is
+ * 
+ * ->gethead of the graph
+ * ->GroupID=1
+ * -->dfs(head)
+ * --> mark the each vertex visited in the path with GroupID
+ * -->repeat the process untill all the vertex are visited
+ * 
+ * 
+ * @author girish
+ *
+ */
+public class  ConnectedComponents  {
+	private Graph<Integer> graph;
+	private DFSRecursive dfs;
+	public ConnectedComponents(Graph<Integer> graph) {
+		this.graph = graph;
+		StartTraversing();
+		
+	}
+	private void StartTraversing(){
+		int vertexnumber=2;
+		dfs = new DFSRecursive(graph);
+		for(int i=1;i<graph.V();i++){
+			dfs.dfs(graph,vertexnumber);
+			vertexnumber+=1;
+		}
+		
+	}
+	/**
+	 * 
+	 * 
+	 * @return the list of the vertex -> GroupID from the dfs Algorithm
+	 */
+	public int[] getindex(){
+		return dfs.getindex();
+	}
+	public boolean isConnected(int i ,int j){
+		return getindex()[i]==getindex()[j];
+	}
+	public static void main(String[] args) {
+		Graph<Integer> g = new Graph<Integer>(5);
+		g.addEdge(1, 2);
+		g.addEdge(2, 3);
+		g.addEdge(5, 4);
+		
+		
+			ConnectedComponents cc = new ConnectedComponents(g);
+			
+			//Test1 -> output = false as 5 and 2 are not connected
+			System.out.println(cc.isConnected(2, 5));
+			//Test2 -> output = true as 1 and 3 are connected
+			System.out.println(cc.isConnected(1, 3));
+	}
+	
+	
+	
+
+}
--- Alogrithm/src/graphs/DFSRecursive.java
+++ Alogrithm/src/graphs/DFSRecursive.java
@@ -1,30 +1,80 @@
 package graphs;
 
+
+
+/**
+ * @author girish Ramnani
+ *
+ */
 public class DFSRecursive {
+	
 	private boolean[] marked;
-	public DFSRecursive(Diagraph<Integer> G, int s){
-		marked = new boolean[G.V()+1];
-		dfs(G,s);
+	private int[] golist;
+	int count=1;
+	private int ConnectComponentGroupID=1;
+	public DFSRecursive(Graph<Integer> graph){
+		marked = new boolean[graph.V()+1];
+		golist = new int[graph.V()+1];
+		dfs(graph,1);
 		
 	}
-	private void dfs(Diagraph<Integer> g, int s) {
-		marked[s]=true;
-		System.out.println("checked "+s);
-		for(Integer w : g.adj(s)){
-			if(!marked[w]) dfs(g,w);
+	
+	/**
+	 * the wrapper method for DFS algorithm so that , the group Id wont increase by 1 for 
+	 * the nodes which have been already visited
+	 * @param graph
+	 * @param i
+	 */
+	public void dfs(Graph<Integer> graph,int i){
+		
+		if (marked[i]==false){
+			golist[i]=ConnectComponentGroupID;
+			dfs(graph,i,ConnectComponentGroupID);
+			ConnectComponentGroupID+=1;
 			
 		}
-		
 	}
+	
+	/**
+	 * 
+	 * this is the recursive dfs algorithm
+	 * 
+	 * @param g -the graph from the Graph API
+	 * @param s  the vertex number
+	 * @param i the group ID
+	 */
+	private void dfs(Graph<Integer> g, int s,int i) {
+		
+		marked[s]=true;
+		for(Integer w : g.adj(s)){
+			if(!marked[w]) {
+				golist[w]=i;
+				dfs(g,w,i);
+			}
+			
+		}
+	}
+	
+	public int[] getindex(){
+		return golist;
+	}
+	public int getCount(){
+		return count;
+	}
+	/**
+	 * here is a the DFS algorithm as to find connected components , we will require it
+	 *  
+	 */
 	public static void main(String[] args) {
-Diagraph<Integer> g = new Diagraph<Integer>(5);
+		
+		Diagraph<Integer> g = new Diagraph<Integer>(5);
 		
 		g.addEdge(1, 2);
 		g.addEdge(2, 3);
 		g.addEdge(5, 4);
-		g.addEdge(3, 5);
-		g.addEdge(5, 1);
-		DFSRecursive x = new DFSRecursive(g, 1);
 		
+		DFSRecursive x = new DFSRecursive(g);
+		
+		 
 	}
 }
