6494e1d7d8fbc50798c66e0edf6d0834730fc14a
girishramnani <girishramnani95@gmail.com>
2015-07-01 20:02:45 +0650

    added a file Intellij_idea_projects/JavaCopyFileRemover/src/com/girish/MainForm.java

---
+++ Intellij_idea_projects/JavaCopyFileRemover/src/com/girish/MainForm.java
@@ -0,0 +1,15 @@
+package com.girish;
+
+/**
+ * Created by Girish on 24-02-2015.
+ */
+public class MainForm {
+    private JPanel panel1;
+    private JButton STARTButton;
+    private JTextField textField1;
+    private JList list1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
